011603ee39f22efd031f97a97ff6b78499a61683
Umut Ozel <umutozel@gmail.com>
2015-07-24 00:16:58 +0300

    jshint warnings cleared

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -2,7 +2,7 @@
 
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope) {})
+.controller('DashCtrl', function() {})
 
 .controller('ChatsCtrl', function($scope, Chats) {
   // With the new view caching in Ionic, Controllers are only called
